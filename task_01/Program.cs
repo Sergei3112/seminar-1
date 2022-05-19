d5ecdfa11f4c403790d3241ef2c93748fd1ad36b
serg <yyy.tre@yandex.ru>
2022-05-19 16:59:48 +0500

    задача 1 к семинару 1

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,29 +1,29 @@
-﻿//Напишите программу, которая на вход
-//принимает два числа и выдаёт, какое число большее, а
-//какое меньшее
+﻿// Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее
 
-    Console.WriteLine( );
-    int a, b;
-    Console.WriteLine("Введи свое первое число - ");
-    a = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введи свое второе число - ");
-    b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int a, b;
+Console.WriteLine("Введи свое первое число - ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи свое второе число - ");
+b = Convert.ToInt32(Console.ReadLine());
 
-    var Chto0 = a > b;
-    var Chto1 = a == b;
-    var Chto2 = a < b;
+var Chto0 = a > b;
+var Chto1 = a == b;
+var Chto2 = a < b;
 
-    if (Chto0)
-    {
-        Console.WriteLine($"Число {a} больше чем {b}");
-    }
-    else if (Chto1)
-    {
-        Console.WriteLine($"Числа {a} и {b} равны");
-    }
-    else
-    {
-        Console.WriteLine(value: $"Число {b} больше чем {a}");
-    }
+if (Chto0)
+{
+    Console.WriteLine($"Число {a} больше чем {b}");
+}
+else if (Chto1)
+{
+    Console.WriteLine($"Числа {a} и {b} равны");
+}
+else
+{
+    Console.WriteLine(value: $"Число {b} больше чем {a}");
+}
 
 
