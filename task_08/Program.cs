e5fb87b4724eca2fccab55e29aeaf0c2747b4aed
serg <yyy.tre@yandex.ru>
2022-05-19 19:35:25 +0500

    задача 19 к семинару 3

--- task_08/Program.cs
+++ task_08/Program.cs
@@ -3,25 +3,27 @@
 // палиндромом.
 
 Console.WriteLine("Задача 15 ");
+
 Console.Write(" введите число ");
 
 bool palindrome = true;
 int x = int.Parse(Console.ReadLine());
 
 if (x < 10000 || x > 99999)
-    
-Console.WriteLine("ошибка!!! номер должен быть пятизначным...");
+
+    Console.WriteLine("ошибка!!! номер должен быть пятизначным...");
 
 int c = x.ToString().Length - 1;
 string b = x.ToString();
 
 for (int i = 0; i < c; i++)
+
     if (b[i] != b[c - i])
         palindrome = false;
-    if (palindrome == true)
-Console.Write(" число является палиндромом ");
+if (palindrome == true)
+    Console.Write(" число является палиндромом ");
 
-else 
+else
 
-Console.Write(" число не является палиндромом ");
+    Console.Write(" число не является палиндромом ");
 Console.ReadKey();
