689e8cbc53ed44c63c70455964ce90b71f9b2533
serg <yyy.tre@yandex.ru>
2022-05-19 19:30:01 +0500

    задача 13 к семинару 2

--- task_06/Program.cs
+++ task_06/Program.cs
@@ -1,14 +1,19 @@
-﻿//Напишите программу, которая выводит
-//третью цифру заданного числа или сообщает, что третьей
-//цифры нет.
+﻿// Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
 
 Console.WriteLine("Задача 13");
+
 Console.Write("Введи число: ");
+
 int anyNumber = Convert.ToInt32(Console.ReadLine());
 string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2){
-  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+
+if (anyNumberText.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + anyNumberText[2]);
 }
-else {
-  Console.WriteLine("-> третьей цифры нет");
+else
+{
+    Console.WriteLine("-> третьей цифры нет");
 }
