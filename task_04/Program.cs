105cc0f9c3623562e22cdf57c5e7ef257a923691
serg <yyy.tre@yandex.ru>
2022-05-19 19:12:46 +0500

    задача 8 к семинару 1

--- task_04/Program.cs
+++ task_04/Program.cs
@@ -1,11 +1,16 @@
-﻿//
+﻿// Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+
+Console.WriteLine("Задача 8 ");
 
 Console.Write("введите  число : ");
+
 int n = int.Parse(Console.ReadLine());
-for(int i = 1; i <= n; i++)
-  
-            if (i % 2 == 0) 
-            {
-                Console.Write(i + " ");
-            }
-        
+
+for (int i = 1; i <= n; i++)
+
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
