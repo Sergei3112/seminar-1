c95eed8ba5d4415fe06b281c917eb39dc3dc01f5
serg <yyy.tre@yandex.ru>
2022-05-19 17:22:41 +0500

    задача 15 к семинару 2

--- task_08/Program.cs
+++ task_08/Program.cs
@@ -1,18 +1,27 @@
-﻿   //Напишите программу, которая принимает на
-//вход пятизначное число и проверяет, является ли оно
-//палиндромом.
-   
-   Console.Write(" введите число ");
-        bool palindrome = true;
-        int x = int.Parse(Console.ReadLine());
-         if (x < 10000 || x > 99999) 
-            Console.WriteLine("ошибка!!! номер должен быть пятизначным...");
-        int c = x.ToString().Length - 1;
-        string b = x.ToString();
-        for (int i = 0; i < c; i++)
-            if (b[i] != b[c - i])
-                palindrome = false;
-        if (palindrome == true)
-            Console.Write(" число является палиндромом ");
-        else Console.Write(" число не является палиндромом ");
-        Console.ReadKey();
+﻿// Напишите программу, которая принимает на
+// вход пятизначное число и проверяет, является ли оно
+// палиндромом.
+
+Console.WriteLine("Задача 15 ");
+Console.Write(" введите число ");
+
+bool palindrome = true;
+int x = int.Parse(Console.ReadLine());
+
+if (x < 10000 || x > 99999)
+    
+Console.WriteLine("ошибка!!! номер должен быть пятизначным...");
+
+int c = x.ToString().Length - 1;
+string b = x.ToString();
+
+for (int i = 0; i < c; i++)
+    if (b[i] != b[c - i])
+        palindrome = false;
+    if (palindrome == true)
+Console.Write(" число является палиндромом ");
+
+else 
+
+Console.Write(" число не является палиндромом ");
+Console.ReadKey();
