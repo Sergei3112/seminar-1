e4d4d68a6364dccc86bcdab9c832ebaaba3215cd
serg <yyy.tre@yandex.ru>
2022-05-19 18:47:26 +0500

    задача 4 к семинару 1

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -1,17 +1,23 @@
-﻿//Напишите программу, которая принимает на
-//вход три числа и выдаёт максимальное из этих чисел.
+﻿// Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
 
-    Console.WriteLine("Введите первое число - ");
-    double a = double.Parse(Console.ReadLine());
-    Console.WriteLine("Введите второе число - ");
-    double b = double.Parse(Console.ReadLine());
-    Console.WriteLine(value: "Введите третье число - ");
-    double c = double.Parse(s: Console.ReadLine());
-    if (a < b) a = b;
-    if (a >= c)
-    {
-    }
-    else
-        a = c;
-    Console.WriteLine("Max = {0}", a);
-    Console.ReadKey();
+Console.WriteLine("Задача 13");
+
+Console.WriteLine("Введите первое число - ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число - ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine(value: "Введите третье число - ");
+double c = double.Parse(s: Console.ReadLine());
+
+if (a < b) a = b;
+if (a >= c)
+{
+}
+else
+    a = c;
+
+Console.WriteLine("Max = {0}", a);
+Console.ReadKey();
