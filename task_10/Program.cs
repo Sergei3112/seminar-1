0063e89e1880d62db650c29305aa3c64b59eab65
serg <yyy.tre@yandex.ru>
2022-05-20 07:57:24 +0500

    задача 23 к семинару 3

---
+++ task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на
+//вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int i = 1;
+
+Console.WriteLine("Введите число N: ");
+
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Число -> Куб");
+
+for (i = 1; i <= n; i++)
+{
+    Console.WriteLine(i + "\t" + i * i * i);
+}
