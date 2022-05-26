70112432b8ceddd3bccb6a780773e63ec6511b51
serg <yyy.tre@yandex.ru>
2022-05-26 13:23:33 +0500

    задача 25 к семинару 4

---
+++ task_11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B. Задачу решить без использования Math.Pow.
+
+Console.Write("Введите натуральное число A -> ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите натуральное число B -> ");
+int B = int.Parse(Console.ReadLine());
+
+int S = 1;
+for (int i = 1; i <= B; i++)
+{
+    S *= A;
+}
+Console.WriteLine($"число A возведенное в натуральную степень B = {S}");
