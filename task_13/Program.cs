35cd4cf42271cb09a53d514c3b02529479488f9b
serg <yyy.tre@yandex.ru>
2022-05-26 13:53:49 +0500

    задача 29 к семинару 4

---
+++ task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая задаёт массив целых чисел 
+//  и выводит их на экран.
+//  Размер массива определяется пользователем.
+
+Console.Write($"Введите размер массива -> ");
+int n = int.Parse(Console.ReadLine());
+Random rang = new Random();
+int[] Array = new int[n];
+
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = new Random().Next(1, 100);
+    Console.Write(Array[i] + " ");
+}
