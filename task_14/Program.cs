61f6d715eea3901b5a109276e98147615c82777c
serg <yyy.tre@yandex.ru>
2022-05-29 11:30:02 +0500

    Задача 34 к семинару 5.

---
+++ task_14/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Задать массив, заполнить случайными положительными трёхзначными числами.
+// Показать количество четных чисел
+
+Console.Write($"Введите размер массива -> ");
+int n = int.Parse(Console.ReadLine());
+
+int[] Array = new int[n];
+int countEven = 0;
+
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = new Random().Next(100, 999);
+    Console.Write(Array[i] + " ");
+}
+Console.WriteLine();
+for (int j = 0; j < Array.Length; j++)
+{
+    if (Array[j] % 2 == 0) countEven = countEven + 1;
+}
+Console.Write($"Четные числа из массива-> {countEven}");
