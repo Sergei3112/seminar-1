9ce4916ccd7870957ecd98ed3e1c0dd9795af225
serg <yyy.tre@yandex.ru>
2022-06-07 18:57:55 +0500

    Задача 41 к семинару 6.

---
+++ task_17/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.Write("Введите числа: ");
+
+int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+int numbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+Console.WriteLine(numbers(array));
+
